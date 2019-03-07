3343b309880af10782853df020d82021968f7799
op123355569 <op123355569@163.com>
2019-03-07 14:45:09 +0800

    1.7.3
    1.7.3

--- HollowGoodsLibrary/src/main/java/com/hg/hollowgoods/Adapter/FastAdapter/Bean/Media.java
+++ HollowGoodsLibrary/src/main/java/com/hg/hollowgoods/Adapter/FastAdapter/Bean/Media.java
@@ -8,11 +8,18 @@ import java.io.Serializable;
  */
 public class Media implements Serializable {
 
+    /**** 原始链接 ****/
     private String oldUrl;
+    /**** 原始名称 ****/
     private String oldName;
+    /**** 完整链接 ****/
     private String url;
+    /**** 本地文件 ****/
     private File file;
+    /**** 是否可以删除 ****/
     private boolean canRemove = true;
+    /**** Tag ****/
+    private Object tag;
 
     public String getOldName() {
         return oldName;
@@ -52,5 +59,13 @@ public class Media implements Serializable {
 
     public void setCanRemove(boolean canRemove) {
         this.canRemove = canRemove;
+    }
+
+    public Object getTag() {
+        return tag;
+    }
+
+    public void setTag(Object tag) {
+        this.tag = tag;
     }
 }
